4ef359d725f85f5a8ca0b50e0c4977ab512e2d82
Lorame <loramebakaboula@gmail.com>
2026-08-16 21:03:17 +0200

    Update CI workflow message after starting data collection

--- scripts/hello_nostradamus.py
+++ scripts/hello_nostradamus.py
@@ -1,3 +1,3 @@
 print("Nostradamus' journey began on July, 25th 🔮 !")
-print("Football data collection will begin very soon ⚽️ !")
+print("Historical competition data for the UEFA Champions League has started ⚽️!")
 print("CI/CD pipeline test successful.")
